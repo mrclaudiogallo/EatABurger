48c8439834aa67e6209278a7f1e1ced1d6fb28a0
Claudio Gallo <cgb1408@outlook.com>
2020-09-20 19:10:04 -0600

    Created and Edited APP

---
+++ db/seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Cheeseburger');
+
+INSERT INTO burgers (burger_name) VALUES ('Quarter Pounder');
+
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,43 @@
+$(function() {
+    $(".change-devour").on("click", function(event) {
+        var id = $(this).data("id");
+        var newDevour = $(this).data("newdevoured");
+
+        var newDevourState = {
+            devoured: newDevour
+        };
+
+        $.ajax("api/burgers" + id, {
+            type: "PUT",
+            data: newDevourState
+        }).then(
+            function() {
+                console.log("changed devour to", newDevour);
+
+                location.reload();
+            }
+        );
+    });
+
+    $(".create-form").on("submit", function(event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
+    
+        var newBurger = {
+            burger_name: $("#newBurger").val().trim(),
+            devoured: 0
+        };
+    
+        // Send the POST request.
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+        function() {
+            console.log("created new burger");
+            // Reload the page to get the updated list
+            location.reload();
+        }
+        );
+    });
+});
